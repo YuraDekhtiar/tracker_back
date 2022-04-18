c045302547bb35ea84b8a964b8d8b11f9bf8124a
yura <yura.zb.ua@gmail.com>
2022-04-18 12:10:44 +0300

    init project

--- app/app.js
+++ app/app.js
@@ -4,28 +4,18 @@ const cors = require('koa-cors');
 const bodyParser = require('koa-bodyparser');
 
 const errorHandler = require('./middleware/error.middleware');
+const error404Handler = require('./middleware/error404.middleware');
 
 
 const PORT = 3000;
 
 const app = new Koa();
 app.use(errorHandler);
+app.use(error404Handler);
 app.use(bodyParser());
 app.use(cors());
 app.use(router.routes());
 
-
-
-app.use(async (ctx, next) => {
-    try {
-        await next();
-        if(ctx.status === 404) ctx.throw(404);
-    } catch (e) {
-        const message = {"message":`route < ${ctx.url} > not found`}
-        ctx.status = 404;
-        ctx.body = message;
-    }
-});
 
 app.listen(PORT, () =>
     console.log(`server start at -> http://127.0.0.1:${PORT}`)

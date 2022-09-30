5d4c16a5590d29b98f948b45f75975e9311e64e9
yura <yura.zb.ua@gmail.com>
2022-09-30 09:10:17 +0300

    add: "add new devices"

--- app/controllers/device.controller.js
+++ app/controllers/device.controller.js
@@ -10,5 +10,11 @@ module.exports = {
         ctx.body = await deviceService.getDeviceStatus(ctx);
         ctx.status = 200;
         next();
+    },
+    addDevice: async (ctx, next) => {
+        const {login, name, password} = ctx.request.body
+        ctx.body = deviceService.addDevice(login, name, password)
+        ctx.status = 200;
+        next();
     }
 }

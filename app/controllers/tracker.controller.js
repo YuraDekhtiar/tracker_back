94d1883c09d689888bd52249fe40a1ae7535cc60
yura <yura.zb.ua@gmail.com>
2022-10-05 09:28:57 +0300

    code refactoring

--- app/controllers/tracker.controller.js
+++ app/controllers/tracker.controller.js
@@ -4,12 +4,16 @@ module.exports = {
     locations: async (ctx, next) => {
         ctx.body = await tracker.getLastLocation(ctx)
         ctx.status = 200;
+
         return next();
     },
     addLocation: async (ctx, next) => {
-        await tracker.addLocation({id: ctx.user.id, ...ctx.request.body})
-        ctx.body = "success"
+        const data = {id: ctx.user.id, ...ctx.request.body};
+        const res = await tracker.addLocation(data)
+
+        ctx.body = res.length
         ctx.status = 200;
+
         return next();
     },
 }

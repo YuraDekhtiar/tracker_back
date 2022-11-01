e9be01090eccea6bd648606f4ceb6509893b1d4f
yura <yura.zb.ua@gmail.com>
2022-11-01 13:01:10 +0200

    deployed on docker

--- app/db/redisDB.js
+++ app/db/redisDB.js
@@ -1,11 +1,10 @@
 const redis = require("redis");
+//const client = redis.createClient();
 
-const client = redis.createClient();
-// const client = redis.createClient({
-//     host: 'redis-server',
-//     port: 6379
-// });
+const client = redis.createClient({
+    url: 'redis://redis'
 
+});
 client.connect().then(() => client.on('error', err => {
     console.log('Error ' + err);
 }));

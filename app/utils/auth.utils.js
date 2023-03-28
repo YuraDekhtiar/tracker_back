4243dc3e3d63a4f89477f867f06c3aeeb725b658
yura <yura.zb.ua@gmail.com>
2023-03-28 13:38:49 +0300

    fixed bug token

--- app/utils/auth.utils.js
+++ app/utils/auth.utils.js
@@ -12,7 +12,7 @@ module.exports = {
 
 function makeAccessToken (payload) {
     const options = {
-        expiresIn: '1m'
+        expiresIn: '10m'
     }
     return jwt.sign(payload, tokenSecret, options);
 }

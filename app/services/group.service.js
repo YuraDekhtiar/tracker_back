298f126e9b17eef7b2d9c13526ac3684f2ef5b64
yura <yura.zb.ua@gmail.com>
2023-05-31 16:38:40 +0300

    init group service

---
+++ app/services/group.service.js
@@ -0,0 +1,110 @@
+const db = require("../models");
+const {where} = require("sequelize");
+const Group = db.group;
+const GroupUser = db.groupUser;
+const GroupDevice = db.groupDevice;
+
+module.exports = {
+    getAll: async () => {
+        return await Group.findAndCountAll().then(r => {
+                return {
+                    totalCount: r.count,
+                    groups: r.rows.map(item => item)
+                }
+            }
+        )
+    },
+    groupsCurrentUser: async (userId) => {
+        return await Group.findAndCountAll({
+            where: {
+                user_id: userId
+            }
+        }).then(r => {
+            return {
+                totalCount: r.count,
+                groups: r.rows.map(item => item)
+            }
+        })
+    },
+    groupById: async (id) => {
+        return await Group.findOne({
+            where: {
+                id: id
+            },
+            include: [{
+                model: db.device,
+                attributes: ['id', 'name'],
+            },
+                {
+                    model: db.user,
+                    attributes: ['id', 'username'],
+                }],
+        })
+    },
+    createGroup: async (name, description) => {
+        if (await isExistGroup(name)) {
+            return null;
+        }
+        return await Group.create({
+            name: name,
+            description: description
+        })
+    },
+    editGroup: async (id, name, description) => {
+        return await Group.update({
+            name: name,
+            description: description,
+        }, {
+            where: {
+                id: id
+            }
+        })
+    },
+    deleteGroupById: async (id) => {
+        return await Group.destroy({
+            where: {
+                id: id
+            }
+        })
+    },
+    addUserToGroup: async (groupId, userId) => {
+        return await GroupUser.create({
+                user_id: userId,
+                group_id: groupId
+            }
+        )
+    },
+    addDeviceToGroup: async (groupId, deviceId) => {
+        return await GroupDevice.create({
+                device_id: deviceId,
+                group_id: groupId
+            }
+        )
+    },
+    deleteUserFromGroup: async (groupId, userId) => {
+        return await GroupUser.destroy({
+                where: {
+                    user_id: userId,
+                    group_id: groupId
+                }
+            }
+        )
+    },
+    deleteDeviceFromGroup: async (groupId, device_id) => {
+        return await GroupDevice.destroy({
+                where: {
+                    device_id: device_id,
+                    group_id: groupId
+                }
+            }
+        )
+    },
+}
+
+async function isExistGroup(name) {
+    return await Group.findOne({
+        where: {
+            name: name
+        }
+    })
+}

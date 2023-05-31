3352354db7b7c407df35fb8a60dde95f10b0769d
yura <yura.zb.ua@gmail.com>
2023-05-31 16:39:00 +0300

    code refactoring

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const {userDal} = require("../dal");
 const {Op} = require("sequelize");
 const bcrypt = require("bcrypt")
 const User = db.user;
@@ -9,91 +10,80 @@ const {saltRounds} = require("../constants");
 module.exports = {
     getAllUsers: async () => {
         return await User.findAndCountAll({
-            attributes: { exclude: ['password'] },
-            include:[{
-                model:Role,
-                through: {attributes:[]},
-                attributes: ['name'],
+            attributes: {exclude: ['password']}, include: [{
+                model: Role, through: {attributes: []}, attributes: ['name'],
             }]
         }).then(r => {
             return {
-                totalCount: r.count,
-                users: r.rows
+                totalCount: r.count, users: r.rows
             }
         })
-    },
-    getUserById: async (id) => {
+    }, getUserById: async (id) => {
         return await User.findOne({
             where: {
                 id: id
-            },
-            attributes: { exclude: ['password'] },
-            include:[{
-                model:Role,
-                through: {attributes:[]},
-                attributes: ['name'],
+            }, attributes: {exclude: ['password']}, include: [{
+                model: Role, through: {attributes: []}, attributes: ['name'],
             }]
         })
-    },
-    getUserByNameOrEmail: async (username, email) => {
+    }, getUserByNameOrEmail: async (username, email) => {
         return await User.findOne({
             where: {
-                [Op.or]: [
-                    {username: username.toLowerCase()},
-                    {email: email.toLowerCase()}
-                ]
+                [Op.or]: [{username: username.toLowerCase()}, {email: email.toLowerCase()}]
             },
         });
-    },
-    createUser: async (username, email, password, role) => {
+    }, createUser: async (username, email, password, role) => {
         let createdUser;
         let roleId;
 
-        await Promise.all([
-            createdUser = await User.create({
-                username: username.toLowerCase(),
-                email: email.toLowerCase(),
-                password: bcrypt.hashSync(password, saltRounds),
-            }),
-            roleId = await Role.findOne({
-                attributes: ['id'],
-                where: {
-                    name: role
-                }
-            }).then(r => r.id)
-        ]);
+        await Promise.all([createdUser = await User.create({
+            username: username.toLowerCase(),
+            email: email.toLowerCase(),
+            password: bcrypt.hashSync(password, saltRounds),
+        }), roleId = await Role.findOne({
+            attributes: ['id'], where: {
+                name: role
+            }
+        }).then(r => r.id)]);
         await UserRole.create({
-            user_id: createdUser.id,
-            role_id: roleId
+            user_id: createdUser.id, role_id: roleId
         });
 
         return createdUser;
-    },
-    deleteUserById: async (id) => {
+    }, deleteUserById: async (id) => {
         return await User.destroy({
             where: {
                 id: id
             }
         })
-    },
-    changePassword: async (id, oldPassword, newPassword) => {
+    }, changePassword: async (id, oldPassword, newPassword) => {
         const user = await User.findOne({
             where: {
                 id: id
             }
         });
 
-        if(!user || !bcrypt.compareSync(oldPassword, user?.password))
-            return null
+        if (!user || !bcrypt.compareSync(oldPassword, user?.password)) return null
         // return id when success
         return await user.update({password: bcrypt.hashSync(newPassword, saltRounds)}).then(r => r.id)
     },
-    lastVisitTimeStamp: async (id) => lastVisitTimeStamp(id)
+    lastVisitTimeStamp: async (id) => {
+        await lastVisitTimeStamp(id)
+    },
+    getAllUsersOutGroup: async (id) => {
+        const users = await userDal.getAllUsersOutGroup(id).then(r => r.map(i => {
+            return {username: i.username, id: i.id}
+        }))
+        return {
+            totalCount: users.length,
+            users: users
+        }
+    },
+
 }
 
-
 async function lastVisitTimeStamp(id) {
-    return await User.update({ last_visit: new Date() }, {
+    return await User.update({last_visit: new Date()}, {
         where: {
             id: id
         }

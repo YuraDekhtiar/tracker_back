9b2ecaabbe85d4556b62429bdb657dc1f835cfe7
yura <yura.zb.ua@gmail.com>
2023-05-31 16:25:23 +0300

    code refactoring

--- app/middlewares/validation.middleware.js
+++ app/middlewares/validation.middleware.js
@@ -3,9 +3,6 @@ module.exports = (schema, validationBody = true) => async (ctx, next) => {
     const { error } = schema.validate(validationBody ? ctx.request.body : ctx.query);
 
     if (error) {
-        console.log("validation.middlewares: ")
-        console.log(error)
-
         ctx.body = error.details[0].message;
         ctx.throw(422);
     }

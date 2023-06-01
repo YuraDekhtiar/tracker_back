ab1a056c1f7c49fc8bb07e3468546e70cf70519e
yura <yura.zb.ua@gmail.com>
2023-06-01 16:37:38 +0300

    fixed bugs of time in database

--- app/dal/device.dal.js
+++ app/dal/device.dal.js
@@ -7,8 +7,7 @@ module.exports = {
         `)
     },
     getAllDevicesByUserId: async (userId) => {
-        return await queryDB(`SELECT de.id, de.login, de.name, de.time_last_connection, 
-            DATE_FORMAT(de.time_last_connection ,  "%Y-%m-%d %T") as time_last_connection_convert, gd.group_id FROM devices AS de 
+        return await queryDB(`SELECT de.id, de.login, de.name, de.time_last_connection, gd.group_id FROM devices AS de 
             JOIN group_devices AS gd ON gd.device_id = de.id WHERE gd.group_id IN 
             (SELECT group_id FROM group_users WHERE user_id = ${userId}) GROUP BY de.id
         `)

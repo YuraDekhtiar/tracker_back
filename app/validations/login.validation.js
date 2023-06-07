be93ab3f6cc96e091e08f366058fce01439e60c0
yura <yura.zb.ua@gmail.com>
2023-06-07 13:08:53 +0300

    refactoring login page

--- app/validations/login.validation.js
+++ app/validations/login.validation.js
@@ -1,10 +1,6 @@
 
 module.exports = (Joi) => Joi.object()
     .keys({
-        username: Joi.string().required()
-            .min(4)
-            .max(64),
-        password: Joi.string().required()
-            .min(8)
-            .max(64)
+        username: Joi.string().required(),
+        password: Joi.string().required(),
     });
